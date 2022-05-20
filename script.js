51eba7a10f30fa61e9bc5fcfa73cb378ed298c75
vajihemansoori <vajihe.mansoori76@gmail.com>
2022-05-20 22:39:39 +0430

    debug project

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 const options = document.querySelector(".options");
 const computerBtn = document.querySelector(".computer");
-const gameOverElement = document.querySelector(".gameover");
 const friendBtn = document.querySelector(".friend");
 const xBtn = document.querySelector(".x");
 const oBtn = document.querySelector(".o");
 const playBtn = document.querySelector(".play");
+const gameOverElement = document.querySelector(".gameover");
 const player = new Object();
 let OPPONENT;
-const xImage = new Image();
-xImage.src = "x.png";
-const oImage = new Image();
-oImage.src = "o.png";
 
 computerBtn.addEventListener("click", function () {
   OPPONENT = "computer";
@@ -26,6 +22,7 @@ oBtn.addEventListener("click", function () {
   player.man = "O";
   player.computer = "X";
   player.friend = "X";
+
   switchActive(xBtn, oBtn);
 });
 
@@ -33,21 +30,24 @@ xBtn.addEventListener("click", function () {
   player.man = "X";
   player.computer = "O";
   player.friend = "O";
+
   switchActive(oBtn, xBtn);
 });
 
 playBtn.addEventListener("click", function () {
   if (!OPPONENT) {
-    computerBtn.style.backgroundColor = "#7700ff ";
-    friendBtn.style.backgroundColor = "#7700ff ";
+    computerBtn.style.backgroundColor = "#7700ff";
+    friendBtn.style.backgroundColor = "#7700ff";
     return;
   }
+
   if (!player.man) {
-    oBtn.style.backgroundColor = "#7700ff ";
+    oBtn.style.backgroundColor = "#7700ff";
     xBtn.style.backgroundColor = "#7700ff";
     return;
   }
-  //   init(player, OPPONENT);
+
+  init(player, OPPONENT);
   options.classList.add("hide");
 });
 
@@ -59,19 +59,23 @@ function switchActive(off, on) {
 function init(player, OPPONENT) {
   const canvas = document.getElementById("cvs");
   const ctx = canvas.getContext("2d");
+  let board = [];
   const COLUMN = 3;
   const ROW = 3;
-  let board = [];
   const SPACE_SIZE = 150;
   let gameData = new Array(9);
   let currentPlayer = player.man;
+  const xImage = new Image();
+  xImage.src = "X.png";
+  const oImage = new Image();
+  oImage.src = "O.png";
   const COMBOS = [
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
   ];
@@ -81,7 +85,7 @@ function init(player, OPPONENT) {
     let id = 0;
     for (let i = 0; i < ROW; i++) {
       board[i] = [];
-      for (let j = 0; i < COLUMN; j++) {
+      for (let j = 0; j < COLUMN; j++) {
         board[i][j] = id;
         id++;
         ctx.strokeStyle = "#000";
@@ -90,7 +94,6 @@ function init(player, OPPONENT) {
     }
   }
   drawBoard();
-
   canvas.addEventListener("click", function (event) {
     if (GAME_OVER) return;
     let X = event.clientX - canvas.getBoundingClientRect().x;
@@ -98,8 +101,10 @@ function init(player, OPPONENT) {
     let i = Math.floor(Y / SPACE_SIZE);
     let j = Math.floor(X / SPACE_SIZE);
     let id = board[i][j];
+
     if (gameData[id]) return;
     gameData[id] = currentPlayer;
+
     drawOnBoard(currentPlayer, i, j);
 
     if (isWinner(gameData, currentPlayer)) {
@@ -107,26 +112,107 @@ function init(player, OPPONENT) {
       GAME_OVER = true;
       return;
     }
+
     if (isTie(gameData)) {
       showGameOver("tie");
       GAME_OVER = true;
       return;
     }
 
-    if (currentPlayer == player.man) {
-      currentPlayer = player.friend;
+    if (OPPONENT == "computer") {
+      let id = minimax(gameData, player.computer).id;
+      gameData[id] = player.computer;
+      let space = getIJ(id);
+
+      drawOnBoard(player.computer, space.i, space.j);
+
+      if (isWinner(gameData, player.computer)) {
+        showGameOver(player.computer);
+        GAME_OVER = true;
+        return;
+      }
+
+      if (isTie(gameData)) {
+        showGameOver("tie");
+        GAME_OVER = true;
+        return;
+      }
     } else {
-      currentPlayer = player.man;
+      currentPlayer = currentPlayer == player.man ? player.friend : player.man;
     }
   });
+
+  function minimax(gameData, PLAYER) {
+    if (isWinner(gameData, player.computer)) return { evaluation: +10 };
+    if (isWinner(gameData, player.man)) return { evaluation: -10 };
+    if (isTie(gameData)) return { evaluation: 0 };
+
+    let EMPTY_SPACES = getEmptySpaces(gameData);
+    let moves = [];
+
+    for (let i = 0; i < EMPTY_SPACES.length; i++) {
+      let id = EMPTY_SPACES[i];
+      let backup = gameData[id];
+      gameData[id] = PLAYER;
+      let move = {};
+      move.id = id;
+      if (PLAYER == player.computer) {
+        move.evaluation = minimax(gameData, player.man).evaluation;
+      } else {
+        move.evaluation = minimax(gameData, player.computer).evaluation;
+      }
+      gameData[id] = backup;
+      moves.push(move);
+    }
+
+    let bestMove;
+
+    if (PLAYER == player.computer) {
+      let bestEvaluation = -Infinity;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].evaluation > bestEvaluation) {
+          bestEvaluation = moves[i].evaluation;
+          bestMove = moves[i];
+        }
+      }
+    } else {
+      let bestEvaluation = +Infinity;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].evaluation < bestEvaluation) {
+          bestEvaluation = moves[i].evaluation;
+          bestMove = moves[i];
+        }
+      }
+    }
+    return bestMove;
+  }
+
+  function getEmptySpaces(gameData) {
+    let EMPTY = [];
+    for (let id = 0; id < gameData.length; id++) {
+      if (!gameData[id]) EMPTY.push(id);
+    }
+
+    return EMPTY;
+  }
+
+  function getIJ(id) {
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] == id) return { i: i, j: j };
+      }
+    }
+  }
 
   function isWinner(gameData, player) {
     for (let i = 0; i < COMBOS.length; i++) {
       let won = true;
+
       for (let j = 0; j < COMBOS[i].length; j++) {
         let id = COMBOS[i][j];
         won = gameData[id] == player && won;
       }
+
       if (won) {
         return true;
       }
@@ -146,15 +232,20 @@ function init(player, OPPONENT) {
   }
 
   function showGameOver(player) {
-    let imgSrc = `${player}.png`;
     let message = player == "tie" ? "Oops No Winner" : "The Winner is";
-    gameOverElement.innerHTML = `<h1>${message}</h1>
-  <img class="winner-img" src=${imgSrc}>
-  <div class="play" onclick="location.reload();">PLAY AGAIN</div>`;
+    let imgSrc = `${player}.png`;
+
+    gameOverElement.innerHTML = `
+            <h1>${message}</h1>
+            <img class="winner-img" src=${imgSrc} </img>
+            <div class="play" onclick="location.reload()">Play Again</div>
+        `;
+
     gameOverElement.classList.remove("hide");
   }
+
   function drawOnBoard(player, i, j) {
-    const img = player == "X" ? xImage : oImage;
+    let img = player == "X" ? xImage : oImage;
     ctx.drawImage(img, j * SPACE_SIZE, i * SPACE_SIZE);
   }
 }
